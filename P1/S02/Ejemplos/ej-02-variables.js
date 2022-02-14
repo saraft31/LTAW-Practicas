70ba1142736546fdeb8a79372faca8b95379e18a
saraft31 <sara.odon.31@gmail.com>
2022-02-14 11:47:17 +0100

    ejemplo 2

---
+++ P1/S02/Ejemplos/ej-02-variables.js
@@ -0,0 +1,20 @@
+//-- Probando variables y cómo imprimirlas
+//-- en la consola
+
+//-- Variable numérica
+let n = 3;
+
+//-- Imprimir la variable directamente
+console.log("Variable n: ", n);
+
+//-- Valor de la variable dentro de una cadena
+console.log(`Variable n: ${n} metros`);
+
+//-- Concatenar la variable al mensaje
+console.log("Variable n: " + n);
+
+//comando terminal $ node ej-02-variables.js 
+// Variable n:  3
+// Variable n: 3 metros
+//Variable n: 3
+
