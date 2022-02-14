02a8b5e86cc280d8d4498c509618d81d5b20def4
saraft31 <sara.odon.31@gmail.com>
2022-02-14 11:42:54 +0100

    primer ej S2

---
+++ P1/S02/Ejemplos/ej-01-hello.js
@@ -0,0 +1,9 @@
+//-- Programa Hola mundo en Node.js
+
+/* Este es un ejemplo de comentario multilínea 
+   El objeto console está directamente disponible 
+   desde node.js, sin tener que importar nada */
+
+//-- Imprimir un mensaje en la consola 
+console . log ( "¡Hola Mundo!" ) ;
+
