69aa625162b6a30fa0347baec4031957b137d7cd
saraft31 <sara.odon.31@gmail.com>
2022-02-15 14:20:22 +0100

    ej 3 + comentarios

---
+++ P1/S03/Ej-03.js
@@ -0,0 +1,43 @@
+//ejemplo 3 programacion asincrona
+const http = require('http');
+
+const PUERTO = 8080;
+
+//-- SERVIDOR: Bucle principal de atención a clientes
+//CUANDO RECIBO UNA SOLICITUD
+const server = http.createServer((req, res) => {
+
+  console.log("\nMENSAJE A")
+
+  req.on('data', (cuerpo) => {
+    console.log("MENSAJE B")
+  }); //cuando lleguen datros
+
+  //eento end
+  req.on('end', ()=> {
+    console.log("MENSAJE C");
+
+    //-- Hayppy server. Generar respuesta
+    res.setHeader('Content-Type', 'text/plain');
+    res.write("Soy el happy server\n");
+    res.end()
+  });
+
+  console.log("MENSAJE D");
+
+});
+
+console.log("MENSAJE E"); //primer mensaje que va a sacar
+server.listen(PUERTO);
+console.log("MENSAJE F");
+
+//-- Arranca servidor 
+//-- MENSAJE E
+//-- MENSAJE F
+//---- Llega una solicitud
+//-- MENSAJE A
+//-- MENSAJE D
+//---- Evento END
+//-- MENSAJE C
+
+// curl "127.0.0.1:8080" solicitud que hago 
