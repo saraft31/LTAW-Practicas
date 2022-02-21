1a37a4bb900f31dd30f42e2fb0d4461bc1746dd1
saraft31 <sara.odon.31@gmail.com>
2022-02-21 11:42:02 +0100

    creo mis ficheros

---
+++ P1/tienda.js
@@ -0,0 +1,23 @@
+// -- el servidor de mi tienda
+
+
+// fichero html 
+// ficheros con img
+// ficheros css
+// devolver fichero pedido
+
+
+// -- PRACTICA 1: TIENDA ON-LINE CON NODE.JS
+
+
+//-- Importar los modulos http, fs y url
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+//-- Definir el puerto a utilizar
+const PUERTO = 9000;
+
+//-- Mensaje de arranque
+console.log("Arrancando servidor...");
+
