7e417749374a0faba3db7fa22e430a81e7e1ecf7
saraft31 <sara.odon.31@gmail.com>
2022-03-01 14:42:59 +0100

    cambios html y pagina error

--- P1/productos.js
+++ P1/productos.js
@@ -1,37 +1,37 @@
 //Constantes de los botones
-const bCasco = document.getElementById('cascos');
+const bCasco = document.getElementById('cascosyprotec');
 const bTablas = document.getElementById('tablas');
 const bFija= document.getElementById('fija');
 const bBotas= document.getElementById('botas');
 
 //oculto los productos inicialmente
-document.getElementById('productos').style.display = 'none';
-//document.getElementById('invertidores').style.display = 'none';
+document.getElementById('cascos').style.display = 'none';
+document.getElementById('botaS').style.display = 'none';
 
 bCasco.onclick = () => {
     
     console.log("activo deslizadores");
-    document.getElementById('productos').style.display = 'block';
-    //document.getElementById('invertidores').style.display = 'none';
+    document.getElementById('cascos').style.display = 'block';
+    document.getElementById('botaS').style.display = 'none';
 }
 
 bTablas.onclick = () => {
     
     console.log("activo deslizadores");
-    document.getElementById('productos').style.display = 'none';
-    //document.getElementById('invertidores').style.display = 'none';
+    document.getElementById('cascos').style.display = 'none';
+    document.getElementById('botaS').style.display = 'none';
 }
 
 bFija.onclick = () => {
     
     console.log("activo deslizadores");
-    document.getElementById('productos').style.display = 'none';
-    //document.getElementById('invertidores').style.display = 'none';
+    document.getElementById('cascos').style.display = 'none';
+    document.getElementById('botaS').style.display = 'none';
 }
 
 bBotas.onclick = () => {
     
     console.log("activo deslizadores");
-    document.getElementById('productos').style.display = 'none';
-    //document.getElementById('invertidores').style.display = 'none';
+    document.getElementById('cascos').style.display = 'none';
+    document.getElementById('botaS').style.display = 'block';
 }
