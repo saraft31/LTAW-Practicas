b8b257aed3bd895361d5f9af6ecbc557e0a15b26
saraft31 <sara.odon.31@gmail.com>
2022-05-12 00:11:27 +0200

    avances

--- P3/chat-client.js
+++ P3/chat-client.js
@@ -1,15 +1,18 @@
 // js del cliente
-
-
 console.log("Ejecutando cliente JS...");
+
+
+const msg = document.getElementById("msg");
 
 //creamos el websocket y establecemos conexión con el serv
 const socket = io();
 
-socket.on("message", (msg)=>{
-    if(msg.includes("Usuarios chat:")){
-        mensajesRecibidos.innerHTML =  "</p>" + msg.split("////")[0];
-      }else{
-        display.innerHTML +=  "</p>" + msg.split("////")[0];
-      }
-    });
+
+//-- Se ha recibido el evento 'hello':
+//-- Es el mensaje de bienvenida del servidor
+socket.on('hello', (msg) => {
+     //-- Mensaje en la consola del navegador
+  console.log("Mensaje del servidor: " + msg);
+  //-- Párrafo display
+  display.innerHTML = msg;
+});
