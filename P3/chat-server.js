b29fbb001d730a2bf542e3e8600f4d4450a2ff53
saraft31 <sara.odon.31@gmail.com>
2022-05-11 22:54:56 +0200

    install y lanzo server

--- P3/chat-server.js
+++ P3/chat-server.js
@@ -1,14 +1,21 @@
 // chat del server js
 
 //dependencias 
+
+
 const express = require('express')
+
+// app web vacía
 const app = express()
+
+//asociar serv http com la
 const http = require('http').Server(app);
+
 //-- Biblioteca socket.io en el lado del servidor
 const io = require('socket.io')(http);
 
 //-- Definir el puerto a utilizar
-const PUERTO = 8080
+const PUERTO = 9090
 
 //-- Lanzar servidor
 http.listen(PUERTO, function(){
