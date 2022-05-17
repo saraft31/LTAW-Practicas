1d52c8af0599e9a2e9d8627b62b5b2cc3c0be84b
saraft31 <sara.odon.31@gmail.com>
2022-05-18 00:13:09 +0200

    index

---
+++ P4/index.js
@@ -0,0 +1,50 @@
+const electron = require('electron');
+const ip = require('ip');
+
+//-- Obtener elementos de la interfaz
+const info1 = document.getElementById("info1");
+const info2 = document.getElementById("info2");
+const info3 = document.getElementById("info3");
+const info4 = document.getElementById("info4");
+const usuarios = document.getElementById("usuarios");
+const btn_test = document.getElementById("btn_test");
+const display = document.getElementById("display");
+
+//-- Acceder a la API de node para obtener la info
+//-- Sólo es posible si nos han dado permisos desde
+//-- el proceso princpal
+//-- Versión de Node
+info1.textContent = process.versions.node;
+//-- Versión de Chrome
+info2.textContent = process.versions.chrome;
+//-- Versión electron
+info3.textContent = process.versions.electron;
+
+//-- Inicializar contador usuarios
+usuarios.innerHTML = 0;
+
+btn_test.onclick = () => {
+    console.log("Botón ON!");
+    //-- Enviar mensaje al proceso principal
+    electron.ipcRenderer.invoke('test', "Probando, probando.... !!");
+}
+
+//-- Mensaje recibido del proceso MAIN para la ip
+electron.ipcRenderer.on('ip', (event, message) => {
+    console.log("Recibida Ip: " + message);
+    info4.innerHTML = message;
+    //-- Generar el codigo qr de la url
+});
+
+//-- Mensaje recibido del proceso MAIN para el numero de usuarios
+electron.ipcRenderer.on('usuarios', (event, message) => {
+    console.log("Recibido numero de usuarios: " + message);
+    usuarios.innerHTML = message;
+});
+
+//-- Mensaje recibido del proceso MAIN de los usuarios
+electron.ipcRenderer.on('msg', (event, message) => {
+    console.log("Recibido numero de usuarios: " + message);
+    display.innerHTML += message + "<br>";
+    display.scrollTop = display.scrollHeight;
+});
