8c17e596eb1527813d3bbf2b8850755ce89bf930
saraft31 <sara.odon.31@gmail.com>
2022-05-19 14:08:24 +0200

    pruebas login

--- P2/tienda.js
+++ P2/tienda.js
@@ -7,6 +7,9 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+//-- fich
+const TIENDA_JSON = fs.readFileSync('tienda.json','utf-8');
+
 //-- Definir el puerto a utilizar
 const PUERTO = 9090;
 
@@ -106,6 +109,54 @@ const server = http.createServer(function (req, res) {
         }
     });
 });
+
+switch (filename) {
+    case '':
+        content = TIENDA;
+        get_cookie(req);
+        break;
+    case 'login':
+    
+        let usuario = myURL.searchParams.get('usuario');
+        let correo = myURL.searchParams.get('correo');
+
+        let info = JSON.parse(TIENDA_JSON);
+        let user1 = info['usuarios'][0]['user'];
+        let correo1 = info['usuarios'][0]['correo'];
+        let name1 = info['usuarios'][0]['nombre'];
+        let name2 = info['usuarios'][1]['nombre'];
+        let user2 = info['usuarios'][1]['user'];
+        let correo2 = info['usuarios'][1]['correo'];
+
+        content = RESPUESTA;
+
+        if (correo==correo1 && usuario==user1) {
+            console.log("Coincide");
+
+            content = content.replace("HTML_EXTRA", "Bienvenido " + name1);
+            mime[type]= "text/html";
+
+        }else if (correo==correo2 && usuario==user2){
+            console.log("Coincide");
+
+            content = content.replace("HTML_EXTRA", "Bienvenido " + name2);
+            mime[type]= "text/html";
+
+        }else{
+            content = content.replace("HTML_EXTRA", "NO ESTA REGISTRADO");
+            mime[type]= "text/html";
+        }
+
+        break;
+
+    //-- Si no es ninguna de las anteriores devolver mensaje de error
+    default:
+        res.setHeader('Content-Type','text/html');
+        res.statusCode = 404;
+        res.write(ERROR);
+        res.end();
+    
+}
 
 
 
